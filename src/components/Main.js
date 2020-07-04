5fc1cccd570d0f9ec61bf925b6f010b9beace824
Sai Chen <chensai0811@gmail.com>
2020-07-04 11:53:53 -0400

    full functionality of react based social network

---
+++ src/components/Main.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Register } from './Register';
+import { Login } from './Login';
+import Home from './Home';
+import { Switch, Route, Redirect } from 'react-router'
+
+export class Main extends React.Component {
+  getLogin = () => {
+    // redirect vs. <Login /> vs. this.props.history('/login')
+    // nigix proxy
+    return this.props.isLoggedIn ? <Redirect to="/home"/> : <Login handleLoginSucceed={this.props.handleLoginSucceed}/>;
+    // return this.props.isLoggedIn ? this.props.history.replace('/home') : <Login handleLoginSucceed={this.props.handleLoginSucceed}/>;
+  }
+
+  getHome = () => {
+    return this.props.isLoggedIn ? <Home/> : <Redirect to="/login"/>;
+  }
+  render() {
+    return (
+      <div className="main">
+        <Switch>
+          <Route exact path="/" render={this.getLogin}/>
+          <Route path="/login" render={this.getLogin}/>
+          <Route path="/register" component={Register}/>
+          <Route path="/home" render={this.getHome}/>
+          <Route render={this.getLogin}/>
+        </Switch>
+      </div>
+    );
+  }
+}
